bd4fe793189058b246daa60af86dd10757e31c54
Shondriane <shondriane.wise@yahoo.com>
2023-04-25 10:54:21 -0400

    starting tailwind css

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,17 +2,28 @@ import React from 'react'
 
 const page = () => {
   return <div> 
+    <h1>Contact Me</h1>
     <form>
+      <div className="w-full flex flex-col">
       <label for="firstName">First Name</label>
       <input id="firstName" type="text" required placeholder="First Name"></input>
+      </div>
+      <div className="w-full flex flex-col">
       <label for="lastName">Last Name</label>
       <input id="lastName" type="text" placeholder="Last Name"></input>
+      </div>
+      <div className="w-full flex flex-col">
       <label for="phone">Phone Number</label>
       <input id="phone" type="tel" required placeholder="Phone Number"></input>
+      </div>
+      <div className="w-full flex flex-col">
       <label for="email">Email</label>
       <input id="email" type="email"  required placeholder="Email"></input>
+      </div>
+      <div className="w-full flex flex-col">
       <label for="comment">Comment</label>
-      <input id="comment" type="comment"  required placeholder="Comment"></input>
+      <textarea className="w-full flex flex-col"rows={5}id="comment" type="comment"  required placeholder="How can I help you?"></textarea>
+      </div>
       <button type ="submit">Submit</button>
     </form>
   </div>
