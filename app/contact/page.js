3a87313c1ee1c1090b0ba8387dc38de3a5c53087
Shondriane <shondriane.wise@yahoo.com>
2023-04-25 11:27:13 -0400

    working on styling

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,28 +1,31 @@
 import React from 'react'
 
 const page = () => {
-  return <div> 
-    <h1>Contact Me</h1>
-    <form>
-      <div className="w-full flex flex-col">
+  return <div className='flex flex-col justify-center my-auto'> 
+    <div className=" flex justify-center p-10 my-auto">
+ 
+    <h1 >Contact Me</h1>
+    </div>
+    <form className="flex flex-col mx-auto justify-center">
+      <div className=" flex flex-col mx-auto ">
       <label for="firstName">First Name</label>
       <input id="firstName" type="text" required placeholder="First Name"></input>
       </div>
-      <div className="w-full flex flex-col">
+      <div className=" flex flex-col mx-auto">
       <label for="lastName">Last Name</label>
       <input id="lastName" type="text" placeholder="Last Name"></input>
       </div>
-      <div className="w-full flex flex-col">
+      <div className=" flex flex-col mx-auto">
       <label for="phone">Phone Number</label>
       <input id="phone" type="tel" required placeholder="Phone Number"></input>
       </div>
-      <div className="w-full flex flex-col">
+      <div className=" flex flex-col mx-auto">
       <label for="email">Email</label>
       <input id="email" type="email"  required placeholder="Email"></input>
       </div>
-      <div className="w-full flex flex-col">
+      <div className=" flex flex-col mx-auto">
       <label for="comment">Comment</label>
-      <textarea className="w-full flex flex-col"rows={5}id="comment" type="comment"  required placeholder="How can I help you?"></textarea>
+      <textarea className="flex flex-col mx-auto"rows={5}id="comment" type="comment"  required placeholder="How can I help you?"></textarea>
       </div>
       <button type ="submit">Submit</button>
     </form>
