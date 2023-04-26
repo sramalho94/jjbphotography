a77ec8e9d86c2978f15d116e9e65a2ff310bc985
Stephan D Ramalho <sramalho@fordham.edu>
2023-04-26 12:04:44 -0400

    more styling and links

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,41 @@
+import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 const NavBar = () => {
-  return <div>NavBar</div>
+  return (
+    <ul className="flex flex-row justify-start space-x-3 bg-gray-100 py-3">
+      <div className="relative h-10 w-10 px-10">
+        <Image src="/logo.svg" fill />
+      </div>
+      <li className="flex flex-col justify-center">
+        <Link
+          href="/"
+          className="font-bold text-lg text-[#D4CAA3] hover:text-[#322B11] hover:underline"
+        >
+          Home
+        </Link>
+      </li>
+      <li className="flex flex-col justify-center">
+        <Link
+          href="/contact"
+          className="font-bold text-lg text-[#D4CAA3] hover:text-[#322B11] hover:underline"
+        >
+          {' '}
+          Contact
+        </Link>
+      </li>
+      <li className="flex flex-col justify-center">
+        <Link
+          href="/portfolio"
+          className="font-bold text-lg text-[#D4CAA3] hover:text-[#322B11] hover:underline"
+        >
+          {' '}
+          Portfolio
+        </Link>
+      </li>
+    </ul>
+  )
 }
 
 export default NavBar
