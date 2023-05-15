ce75f31e096bfd5719cc2cbb7538233bbaffacc7
Shondriane <shondriane.wise@yahoo.com>
2023-05-15 13:18:36 -0400

    layout for portfolio

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Landing from '@/components/Landing'
 import Footer from '@/components/Footer'
+import Portfolio from '@/components/Portfolio'
 export default function Home() {
   return (
     <main className="flex  flex-col items-center justify-between p-24 h-screen">
       <Landing/>
+     
       <Footer/>
     </main>
   )
--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Portfolio = () => {
-  return <div>Portfolio</div>
+  const images = [
+    {
+      src: '/banner7.jpg',
+      alt: 'Wedding',
+      link: '/wedding',
+    },
+    {
+      src: '/banner8.jpg',
+      alt: 'Maternity',
+      link: '/maternity',
+    },
+    {
+      src: '/banner6.jpg',
+      alt: 'Candid',
+      link: '/candid',
+    },
+    // Add more images as needed
+  ];
+
+  return (
+    <div className=" mt-8 mb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      {images.map((image, index) => (
+        <div key={index} className="relative">
+          <Image src={image.src} alt={image.alt} layout="responsive"  width={300} height={300} />
+          <Link href={image.link} className="absolute bottom-0 left-0 right-0 bg-gray-800 bg-opacity-50 text-white text-center py-2">
+            {image.alt}
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
+
+
+
+
+
+
+
