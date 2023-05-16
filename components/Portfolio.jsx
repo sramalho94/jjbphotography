b3c1bafe35254ad3216f250a94c9b35751902386
Shondriane <shondriane.wise@yahoo.com>
2023-05-16 10:41:33 -0400

    added portfolio to page

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -25,7 +25,7 @@ const Portfolio = () => {
     <div className=" mt-8 mb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {images.map((image, index) => (
         <div key={index} className="relative">
-          <Image src={image.src} alt={image.alt} layout="responsive"  width={300} height={300} />
+          <Image src={image.src} alt={image.alt} responsive  width={300} height={300} />
           <Link href={image.link} className="absolute bottom-0 left-0 right-0 bg-gray-800 bg-opacity-50 text-white text-center py-2">
             {image.alt}
           </Link>
