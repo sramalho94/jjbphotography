fc551da1403e3972488b8be0f96607c2dc5ab15b
Stephan D Ramalho <sramalho@fordham.edu>
2023-05-24 15:53:14 -0400

    tweak to Image props in Portfolio

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -25,14 +25,23 @@ const Portfolio = () => {
     <div className=" mt-8 mb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
       {images.map((image, index) => (
         <div key={index} className="relative">
-          <Image src={image.src} alt={image.alt} layout="responsive"  width={300} height={300} />
-          <Link href={image.link} className="absolute bottom-0 left-0 right-0 bg-gray-800 bg-opacity-50 text-white text-center py-2">
+          <Image
+            src={image.src}
+            alt={image.alt}
+            responsive
+            width={300}
+            height={300}
+          />
+          <Link
+            href={image.link}
+            className="absolute bottom-0 left-0 right-0 bg-gray-800 bg-opacity-50 text-white text-center py-2"
+          >
             {image.alt}
           </Link>
         </div>
       ))}
     </div>
-  );
+  )
 }
 
 export default Portfolio
