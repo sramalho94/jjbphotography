93285d27e7e96e6a8afc3f1ac81f0b71d4265fc9
Shondriane <shondriane.wise@yahoo.com>
2023-06-01 06:01:51 -0400

    updated styling for aboutMe component

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 const AboutMe = () => {
   return (
-    <div>
-      <span>About Me</span>
+    <div className="text-center">
+      <span className="font-bold">About Me</span>
       <div className="my-3">
         <div className="flex flex-col md:flex-row">
-          <div className="bg-red-600 h-32 w-32"></div>
+          <div className="bg-red-600 h-32 w-32 mx-auto mb-8"></div>
           <div className=" px-3 md:flex-1">
-            <p className="text-sm">
+            <p className="text-sm text-left">
               I have a unique passion for capturing the essence of those who
               often feel unseen. Through my photography, I create a space where
               every moment, big or small, is beautifully preserved, just as it
