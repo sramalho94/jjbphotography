bd9c20a720cf8dcf405f2c5e3d1bac5da1c4db16
Stephan D Ramalho <sramalho@fordham.edu>
2023-10-06 11:40:20 -0400

    changes to padding

--- app/page.js
+++ app/page.js
@@ -5,9 +5,8 @@ import Portfolio from '@/components/Portfolio'
 import AboutMe from '@/components/AboutMe'
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between  min-h-screen md:p-24">
+    <main className="flex flex-col items-center justify-between  min-h-screen p-5 bg-dark-green">
       <Landing />
-
       <Portfolio />
       <AboutMe />
     </main>
