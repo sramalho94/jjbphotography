c79dd0f5e51cbbd5ba0491e8ea0833c7966f4507
Stephan D Ramalho <sramalho@fordham.edu>
2023-10-06 12:04:06 -0400

    added next fonts and applied font to landing component

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -2,6 +2,9 @@
 
 import React, { useEffect, useState } from 'react'
 import { FaChevronCircleLeft, FaChevronCircleRight } from 'react-icons/fa'
+import { Great_Vibes } from 'next/font/google'
+
+const great_vibes = Great_Vibes({ subsets: ['latin'], weight: '400' })
 
 const slides = [
   '/banner1.jpeg',
@@ -32,7 +35,9 @@ const Landing = () => {
   return (
     <div className="relative flex flex-col items-center justify-center h-auto ">
       <div className="absolute text-lg  z-10 left-0 -mt-10 h-full">
-        <h1 className="text-3xl font-bold mb-30 md:text-4xl text-our-beige">
+        <h1
+          className={`text-3xl font-GreatVibesRegular font-extrabold mb-30 md:text-4xl text-our-beige shadow-xl ${great_vibes.className}`}
+        >
           JJB Photography
         </h1>
       </div>
@@ -50,11 +55,11 @@ const Landing = () => {
               alt=""
             />
             <FaChevronCircleLeft
-              className="circle text-silver absolute top-1/2 left-0 transform -translate-y-1/2 -translate-x-1/2"
+              className="circle text-silver absolute top-1/2 left-0 transform -translate-y-1/2 -translate-x-1/2 text-our-beige"
               onClick={prevStep}
             />
             <FaChevronCircleRight
-              className="chevron text-silver absolute top-1/2 right-0 transform -translate-y-1/2 translate-x-1/2"
+              className="chevron text-silver absolute top-1/2 right-0 transform -translate-y-1/2 translate-x-1/2 text-our-beige"
               onClick={nextStep}
             />
           </div>
