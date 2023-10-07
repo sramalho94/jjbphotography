34969315f5dfba7bbef79aeaa4fc0486e3e76774
Stephan D Ramalho <sramalho@fordham.edu>
2023-10-07 11:48:11 -0400

    more styling for Portfolio

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import { Great_Vibes } from 'next/font/google'
+import { Great_Vibes, Bodoni_Moda } from 'next/font/google'
 
 const great_vibes = Great_Vibes({ subsets: ['latin'], weight: '400' })
+const bodoni_moda = Bodoni_Moda({ subsets: ['latin'], weight: '600' })
 const Portfolio = () => {
   const images = [
     {
@@ -31,7 +32,7 @@ const Portfolio = () => {
   return (
     <div>
       <h1
-        className={`mt-8 ${great_vibes.className} font-extrabold text-2xl text-our-beige`}
+        className={`mt-8 ${great_vibes.className} font-extrabold text-2xl text-our-beige text-center`}
       >
         When you step in front of my camera, know that I will honor your story
         and create a visual narrative that truly reflects who you are.
@@ -39,7 +40,10 @@ const Portfolio = () => {
 
       <div className="flex flex-wrap justify-center ">
         {images.map((image, index) => (
-          <div key={index} className="relative h-52 w-52 m-2 md:h-64 md:w-64">
+          <div
+            key={index}
+            className="relative h-52 w-52 m-2 md:h-64 md:w-64 md:p-1 border border-our-beige"
+          >
             <Image
               src={image.src}
               alt={image.alt}
@@ -50,7 +54,7 @@ const Portfolio = () => {
             />
             <Link
               href={image.link}
-              className="absolute bottom-0 left-0 right-0 bg-gray-800 bg-opacity-50 text-white text-center py-2"
+              className={`absolute  bg-our-beige bg-opacity-80 text-dark-green text-center mt-48 ml-32 md:mt-60 md:ml-44 ${bodoni_moda.className}`}
             >
               {image.alt}
             </Link>
