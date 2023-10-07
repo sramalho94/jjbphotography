1918a6fa840829f1a11fcd5f5f8c79b2a3fef199
Stephan D Ramalho <sramalho@fordham.edu>
2023-10-07 11:24:06 -0400

    removed unecessary components from page.js

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Landing from '@/components/Landing'
 import Footer from '@/components/Footer'
 import Portfolio from '@/components/Portfolio'
 import AboutMe from '@/components/AboutMe'
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between  min-h-screen bg-dark-green">
